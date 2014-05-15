9e265e0e74bf30e05eb4d1aa0478dcdc3bc50c50
drw86312 <david.warner4@gmail.com>
2014-05-15 12:57:59 -0500

    Added website name in navigation bar

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -9,11 +9,18 @@
 #import "ViewController.h"
 
 @interface ViewController () <UIWebViewDelegate, UIAlertViewDelegate, UIScrollViewDelegate>
-@property (weak, nonatomic) IBOutlet UIWebView *myWebView;
-@property (weak, nonatomic) IBOutlet UITextField *myURLTextField;
+
+#pragma UI Properties
 @property (weak, nonatomic) IBOutlet UIButton *backButtonOutlet;
 @property (weak, nonatomic) IBOutlet UIButton *forwardButtonOutlet;
+@property (weak, nonatomic) IBOutlet UINavigationItem *navigationBar;
+@property (weak, nonatomic) IBOutlet UITextField *myURLTextField;
+@property (weak, nonatomic) IBOutlet UIWebView *myWebView;
+
+#pragma Other properties
 @property (nonatomic) CGFloat originy;
+@property (weak, nonatomic) NSString *returnedURLString;
+@property (weak, nonatomic) NSString *navigationBarString;
 
 @end
 
@@ -37,6 +44,7 @@
 
 
 }
+#pragma Button actions
 
 - (IBAction)onBackButtonPressed:(UIButton *)sender {
     if ([self.myWebView canGoBack]) {
@@ -54,12 +62,20 @@
 - (IBAction)onReloadButtonPressed:(UIButton *)sender {
     [self.myWebView reload];
 }
+- (IBAction)onDeleteButtonPressed:(UIButton *)sender
+{
+    self.myURLTextField.text = nil;
+}
+
+
 
 -(void)webViewDidFinishLoad:(UIWebView *)myWebView
 {
     [self disableEnableButtonMethod];
-    NSString *labelURLstring = self.myWebView.request.URL.absoluteString;
-    [self.myURLTextField setText:labelURLstring];
+    self.returnedURLString = self.myWebView.request.URL.absoluteString;
+    [self.myURLTextField setText:self.returnedURLString];
+    [self getWebsiteTitle];
+    self.navigationBar.title = self.navigationBarString;
 }
 
 -(BOOL)textFieldShouldReturn:(UITextField *)myURLTextField
@@ -88,7 +104,7 @@
 
 }
 
-// helper method
+#pragma helper method
 
 -(void) disableEnableButtonMethod
 {
@@ -119,6 +135,8 @@
         NSString *urlString = self.myURLTextField.text;
         NSURL *url = [NSURL URLWithString:urlString];
         NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+
         [self.myWebView loadRequest:request];
     }
     else {
@@ -129,6 +147,34 @@
     }
 
 [self.myURLTextField resignFirstResponder];
+}
+
+-(void)getWebsiteTitle
+{
+    NSURL *url = [NSURL URLWithString:self.returnedURLString];
+    NSString *webData= [NSString stringWithContentsOfURL:url];
+    NSString *search1 = @"<title>";
+    NSRange range1 = [webData rangeOfString:search1];
+    NSInteger location1 = (range1.location) + 7;
+
+    NSString *search2 = @"</title>";
+    NSRange range2 = [webData rangeOfString:search2];
+    NSInteger location2 = (range2.location);
+
+    NSInteger length = location2 - location1;
+
+    NSRange range3 = NSMakeRange(location1, length);
+    self.navigationBarString = [webData substringWithRange:range3];
+
+    NSLog(@"%@", self.navigationBarString);
+
+
+//    NSString *substring = [[webData substringFromIndex:NSMaxRange(range)] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+
+//    NSLog(@"%@", webData);
+
 
 }
+
+
 @end
